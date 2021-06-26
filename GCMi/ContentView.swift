b507b8b4ab69c97184de02eb3c9fc1f988deaa11
Brendan <brendan@brendanperry.me>
2021-06-26 02:34:56 -0400

    created xcode project

---
+++ GCMi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GCMi
+//
+//  Created by Brendan Perry on 6/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GCMi/GCMiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GCMiApp.swift
+//  GCMi
+//
+//  Created by Brendan Perry on 6/26/21.
+//
+
+import SwiftUI
+
+@main
+struct GCMiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
